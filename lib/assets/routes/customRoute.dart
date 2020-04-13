97d3e0fb386e00d6e1b8f1a0fd2424336954d63a
Jelgar <jamesnelgar@gmail.com>
2020-04-13 11:37:36 +0100

    Added custom router

---
+++ lib/assets/routes/customRoute.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomRoute<T> extends MaterialPageRoute<T> {
+  CustomRoute({ WidgetBuilder builder, RouteSettings settings })
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    if (settings.isInitialRoute)
+      return child;
+    // Fades between routes. (If you don't want any animation,
+    // just return child.)
+    return new FadeTransition(opacity: animation, child: child);
+  }
+}
