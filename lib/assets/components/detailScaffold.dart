780be81770f967daa59dc9cb563c518fcc2ef6c4
Jelgar <jamesnelgar@gmail.com>
2020-05-12 19:45:28 +0100

    Working collapsing header image

---
+++ lib/assets/components/detailScaffold.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class DetailScaffold extends StatefulWidget {
+  final ScrollController controller;
+  final ScrollPhysics physics;
+  final List<Widget> slivers;
+
+  final double expandedHeight;
+
+  /// Changes edge behavior to account for [SliverAppBar.pinned].
+  ///
+  /// Hides the edge when the [ScrollController.offset] reaches the collapsed
+  /// height of the [SliverAppBar] to prevent it from overlapping the app bar.
+  final bool hasPinnedAppBar;
+
+  DetailScaffold({
+    @required this.expandedHeight,
+    this.controller,
+    this.physics,
+    this.slivers,
+    this.hasPinnedAppBar = false,
+  }) {
+    assert(expandedHeight != null);
+    assert(hasPinnedAppBar != null);
+  }
+
+  @override
+  _DetailScaffoldState createState() => _DetailScaffoldState();
+}
+
+class _DetailScaffoldState extends State<DetailScaffold> {
+  ScrollController ctrl;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ctrl = widget.controller ?? ScrollController();
+    ctrl.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) {
+      ctrl.dispose();
+    }
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        CustomScrollView(
+          controller: ctrl,
+          physics: widget.physics,
+          slivers: widget.slivers,
+        ),
+        _buildEdge(),
+      ],
+    );
+  }
+
+  _buildEdge() {
+    var edgeHeight = 32.0;
+    var paddingTop = MediaQuery.of(context).padding.top;
+
+    var defaultOffset =
+        (paddingTop + widget.expandedHeight) - edgeHeight;
+
+    var top = defaultOffset;
+    var edgeSize = edgeHeight;
+
+    if (ctrl.hasClients) {
+      double offset = ctrl.offset;
+      top -= offset > 0 ? offset : 0;
+
+      if (widget.hasPinnedAppBar) {
+        // Hide edge to prevent overlapping the toolbar during scroll.
+        var breakpoint =
+            widget.expandedHeight - kToolbarHeight - edgeHeight;
+
+        if (offset >= breakpoint) {
+          edgeSize = edgeHeight - (offset - breakpoint);
+          if (edgeSize < 0) {
+            edgeSize = 0;
+          }
+
+          top += (edgeHeight - edgeSize);
+        }
+      }
+    }
+
+    return Positioned(
+      top: top,
+      left: 0,
+      right: 0,
+      child: Container(
+        height: edgeSize,
+        decoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(32),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/other/ActionInfo.dart
+++ lib/pages/other/ActionInfo.dart
@@ -1,3 +1,4 @@
+import 'package:app/assets/components/textButton.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app/models/Action.dart';
@@ -9,6 +10,7 @@ import 'package:app/pages/campaign/CampaignInfo/CampaignInfo.dart';
 import 'package:app/pages/other/RewardComplete.dart';
 
 import 'package:app/assets/components/selectionItem.dart';
+import 'package:app/assets/components/detailScaffold.dart';
 import 'package:app/assets/components/darkButton.dart';
 import 'package:app/assets/routes/customRoute.dart';
 
@@ -24,6 +26,8 @@ class ActionInfo extends StatefulWidget {
 }
 
 class _ActionInfoState extends State<ActionInfo> {
+  final double expandedHeight = 400.0;
+
   CampaignAction _action;
   // Action's parent campaign
   Campaign _campaign;
@@ -39,118 +43,122 @@ class _ActionInfoState extends State<ActionInfo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget> [
-              // Header
-              SliverAppBar(
-
-              bottom: 
-                PreferredSize(
-                  preferredSize: Size.fromHeight(18),  
-                  child: Text(''),
-                ), 
-              expandedHeight: 400.0,
-              floating: false,
-              pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("Action", style: Theme.of(context).primaryTextTheme.display1,),
-                  background: Hero(
-                    tag: "CampaignHeaderImage${_campaign.getId()}",
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: new DecorationImage(
-                          image: new NetworkImage(_campaign.getHeaderImage()),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ];
-         }, 
-          // This is the body for the nested scroll view
-         body: 
-          ListView(
-            children: <Widget>[
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 20,
-                  ),
-                  child: DarkButton(
-                    "View full campaign",
-                    onPressed: () {
-                      Navigator.push(
-                       context,
-                       CustomRoute(builder: (context) => CampaignInfo(campaign: _campaign, model: _model))
-                      );
-                    },
-                  )
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 35, vertical: 10),
-                child: Text(
-                  _action.getTitle(),
-                  style: Theme.of(context).primaryTextTheme.headline,
-                ),
-              ),
-              _model.user.isCompleted(_action) ?
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        height: MediaQuery.of(context).size.width * 0.1 ,
-                        child: Image(image: AssetImage('assets/imgs/partypopperemoji.png'),),
-                      ),
-                      Text("You have completed this action!")
-                       
-                    ],     
-                  ),
-                )
-                :
-                Container(height: 0),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          child: RichText(
-                              text: TextSpan(text: _action.getDescription(), style: Theme.of(context).primaryTextTheme.body1),
-                              ),
-                        )
-                    ]
-              ),
-            ], 
-          ),
-          
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(_action.getTitle()),
         ),
-        floatingActionButton:
-          Padding (
-              padding: EdgeInsets.all(14),
-              child: DarkButton(
-                "Complete Action",
-                onPressed: () {
-                  setState(() {
-                    // TODO Somehow somewhere navigate to completed reward page if reward completed
-                    List<Reward> newlyCompletedRewards = widget.model.user.newlyCompletedRewards(_action);
-                    _model.onCompleteAction(_action, );
-                    if (newlyCompletedRewards.length > 0) {
-                      Navigator.push(
-                        context, 
-                        CustomRoute(builder: (context) => RewardCompletePage(widget.model, newlyCompletedRewards))
-                      );
-                    }
-                  });
-                },
-              )
-          )
-        ,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          // This is the body for the nested scroll view
+         body: DetailScaffold(
+            expandedHeight: expandedHeight,
+            slivers: <Widget>[
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                expandedHeight: expandedHeight,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    color: Colors.purple
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                sliver: 
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        // Button to campaign
+                        //Center(
+                        //  child: Padding(
+                        //    padding: EdgeInsets.only(
+                        //      bottom: 20,
+                        //    ),
+                        //    child: DarkButton(
+                        //      "View full campaign",
+                        //      onPressed: () {
+                        //        Navigator.push(
+                        //         context,
+                        //         CustomRoute(builder: (context) => CampaignInfo(campaign: _campaign, model: _model))
+                        //        );
+                        //      },
+                        //    )
+                        //  ),
+                        //),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Text(
+                            _action.getTitle(),
+                            style: Theme.of(context).primaryTextTheme.headline1,
+                            textAlign: TextAlign.left
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 0, vertical: 5),
+                          child: TextButton(
+                            _campaign.getTitle(),
+                            onClick: () {
+                              Navigator.push(
+                               context,
+                               CustomRoute(builder: (context) => CampaignInfo(campaign: _campaign, model: _model))
+                              );
+                            },
+                          )
+                        ),
+                        Container(height: 20),
+                        _model.user.isCompleted(_action) ?
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Row(
+                              children: <Widget>[
+                                Container(
+                                  height: MediaQuery.of(context).size.width * 0.1 ,
+                                  child: Image(image: AssetImage('assets/imgs/partypopperemoji.png'),),
+                                ),
+                                Text("You have completed this action!")
+                                 
+                              ],     
+                            ),
+                          )
+                          :
+                          Container(height: 0),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                                  Expanded(
+                                    child: RichText(
+                                        text: TextSpan(text: _action.getDescription(), style: Theme.of(context).primaryTextTheme.body1),
+                                        ),
+                                  )
+                              ]
+                        ),
+                      ],
+                    )
+                  )
+                ,
+              ),
+
+            ],
+          ),
+          //floatingActionButton:
+          //Padding (
+          //    padding: EdgeInsets.all(14),
+          //    child: DarkButton(
+          //      "Complete Action",
+          //      onPressed: () {
+          //        setState(() {
+          //          // TODO Somehow somewhere navigate to completed reward page if reward completed
+          //          List<Reward> newlyCompletedRewards = widget.model.user.newlyCompletedRewards(_action);
+          //          _model.onCompleteAction(_action, );
+          //          if (newlyCompletedRewards.length > 0) {
+          //            Navigator.push(
+          //              context, 
+          //              CustomRoute(builder: (context) => RewardCompletePage(widget.model, newlyCompletedRewards))
+          //            );
+          //          }
+          //        });
+          //      },
+          //    )
+          //)
+          //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
