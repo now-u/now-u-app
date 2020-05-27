db2d8acf500c4933d44cc19ef0a1a95635fd29bd
Jelgar <jamesnelgar@gmail.com>
2020-05-27 21:01:22 +0100

    Updated learning centre

--- lib/pages/campaign/LearningCentre/LearningCentrePage.dart
+++ lib/pages/campaign/LearningCentre/LearningCentrePage.dart
@@ -1,3 +1,5 @@
+import 'package:app/assets/StyleFrom.dart';
+import 'package:app/assets/components/textButton.dart';
 import 'package:flutter/material.dart';
 
 import 'package:redux/redux.dart';
@@ -10,6 +12,10 @@ import 'package:app/models/State.dart';
 
 import 'package:app/assets/components/customAppBar.dart';
 import 'package:app/assets/components/selectionItem.dart';
+
+const double CIRCLE_1_RADIUS = 150;
+const double CIRCLE_2_RADIUS = 250;
+const double HEADING_HEIGHT = 250;
 
 class LearningCentrePage extends StatelessWidget {
 
@@ -25,35 +31,93 @@ class LearningCentrePage extends StatelessWidget {
           return FutureBuilder(
             future: viewModel.api.getLearningCentre(campaignId),
             builder: (context, snapshot) {
-              return CustomScrollView(
-                slivers: <Widget>[
-                  SliverAppBar(
-                    backgroundColor: Colors.white,
-                    expandedHeight: 300,
-                    automaticallyImplyLeading: false,
-                    flexibleSpace: Stack(
+              return 
+              !snapshot.hasData ? 
+              Center(child: CircularProgressIndicator())
+              :
+              Column(
+                children: <Widget>[
+                  Container(
+                    color: Colors.white,
+                    height: HEADING_HEIGHT,
+                    child: Stack(
                       children: [
+                        Positioned( 
+                          top: -40,
+                          left: MediaQuery.of(context).size.width * 0.5 - 40,
+                          child: Container(
+                            height: CIRCLE_1_RADIUS,
+                            width: CIRCLE_1_RADIUS,
+                            decoration: BoxDecoration(
+                              color: colorFrom(
+                                Theme.of(context).primaryColor,
+                                opacity: 0.2,
+                              ),
+                              borderRadius: BorderRadius.circular(CIRCLE_1_RADIUS/2),
+                            ),
+                          )
+                        ),
+                        Positioned( 
+                          top: 120,
+                          left: -80,
+                          child: Container(
+                            height: CIRCLE_2_RADIUS,
+                            width: CIRCLE_2_RADIUS,
+                            decoration: BoxDecoration(
+                              color: colorFrom(
+                                Theme.of(context).primaryColor,
+                                opacity: 0.2,
+                              ),
+                              borderRadius: BorderRadius.circular(CIRCLE_2_RADIUS/2),
+                            ),
+                          )
+                        ),
                         Align(
                           alignment: Alignment.bottomRight,
-                          child: Container(
-                            height: 200,
-                            width: 200,
-                            child: Image.asset(
-                              "assets/imgs/learning.png",
-                              width: 200,
-                              height: 200,
-                            ),
+                          child: Image.asset(
+                            "assets/imgs/learning.png",
+                            height: 180,
                           ),
-                        )
+                        ),
+                        SafeArea(
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Align(
+                              alignment: Alignment.topRight,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  TextButton(
+                                    "Menu",
+                                    onClick: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    iconLeft: true,
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    "Learning centre",
+                                    style: Theme.of(context).primaryTextTheme.headline2,
+                                  )
+                                ],
+                              ),
+                            )
+                          )
+                        ),
                       ],
                     ),
                   ),
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate ( (context, index) {
-                      return LearningTopicSelectionItem(
-                        topic: snapshot.data.getLearningTopics()[index],
-                      );
-                    }, childCount: snapshot.data.getLearningTopics().length ),
+                  Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.getLearningTopics().length,
+                      itemBuilder: (context, index) {
+                        return LearningTopicSelectionItem(
+                          topic: snapshot.data.getLearningTopics()[index],
+                        );
+                      },
+                    )
                   )
                 ],
               );
