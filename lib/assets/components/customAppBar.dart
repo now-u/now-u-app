63781656fde433c8bada35ee8d07d534d94eeebd
jelgar <jamesnelgar@gmail.com>
2020-06-21 17:24:57 +0100

    Fixed Padding before leader in app bar

--- lib/assets/components/customAppBar.dart
+++ lib/assets/components/customAppBar.dart
@@ -2,60 +2,72 @@ import 'package:flutter/material.dart';
 
 import 'package:app/assets/components/textButton.dart';
 
-AppBar CustomAppBar(
-    {
-      @required text,
-      @required context,
-      String backButtonText,
-      bool hasBackButton,
-      List<Widget> actions,
-      Function extraOnTap,
-    }
-    ) {
+AppBar CustomAppBar({
+  @required text,
+  @required context,
+  String backButtonText,
+  bool hasBackButton,
+  List<Widget> actions,
+  Function extraOnTap,
+}) {
   hasBackButton = hasBackButton == null ? true : hasBackButton;
   return AppBar(
-   backgroundColor: Colors.white,
-   automaticallyImplyLeading: false,
-   centerTitle: true,
-   title: 
-    Row(
+    //leading: SizedBox(width: 0),
+    backgroundColor: Colors.white,
+    automaticallyImplyLeading: false,
+    centerTitle: true,
+    title: Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        Flexible(
-          flex: 1,
-          child: 
-          !hasBackButton ? Container() :
-          TextButton(
-            backButtonText ?? "Back",
-            iconLeft: true,
-            onClick: (){
-              if (extraOnTap != null) {
-                extraOnTap();
-              }
-              Navigator.pop(context);
-            },
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: !hasBackButton
+                ? Container()
+                : Container(
+                    child: TextButton(
+                      backButtonText ?? "Back",
+                      iconLeft: true,
+                      onClick: () {
+                        if (extraOnTap != null) {
+                          extraOnTap();
+                        }
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
           ),
         ),
-        Container(
-          child: Text(
-            text, 
-            style: TextStyle(
-              color: Theme.of(context).primaryTextTheme.headline4.color,
-              fontSize: Theme.of(context).primaryTextTheme.headline4.fontSize,
-              fontWeight: FontWeight.w500,
-              fontStyle: Theme.of(context).primaryTextTheme.headline4.fontStyle,
-              fontFamily: Theme.of(context).primaryTextTheme.headline4.fontFamily,
-            ),
-          )
+        Text(
+          text,
+          style: TextStyle(
+            color: Theme.of(context).primaryTextTheme.headline4.color,
+            fontSize: Theme.of(context).primaryTextTheme.headline4.fontSize,
+            fontWeight: FontWeight.w500,
+            fontStyle: Theme.of(context).primaryTextTheme.headline4.fontStyle,
+            fontFamily: Theme.of(context).primaryTextTheme.headline4.fontFamily,
+          ),
         ),
-        Flexible(
-          flex: 1,
-          child: Container(),
-        )
+        Expanded(
+          child: Container(
+              //flex: 1,
+              child: actions.length == 0
+                  ? Container()
+                  : Align(
+                      alignment: Alignment.centerRight,
+                      child: actions.length == 1
+                          ? actions[0]
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: actions,
+                            ),
+                    )),
+        ),
       ],
     ),
-    actions: actions,
+    //actions: actions,
   );
 }
