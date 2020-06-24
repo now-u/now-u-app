9c89c09ab63f64b82a2740b250938402f1410d84
jelgar <jamesnelgar@gmail.com>
2020-06-24 22:40:18 +0100

    Updated partners page

--- lib/assets/components/header.dart
+++ lib/assets/components/header.dart
@@ -1,35 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:app/assets/components/textButton.dart';
 
 class PageHeader extends StatelessWidget {
   final Function onTap;
   final String title;
   final IconData icon;
-  PageHeader({
-    this.onTap,
-    this.title,
-    this.icon,
-  });
+  final bool backButton;
+  final double padding;
+  PageHeader(
+      {this.onTap, this.title, this.icon, this.backButton, this.padding});
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         SafeArea(
-          child: Align(
-            alignment: Alignment.centerRight,
-            child: GestureDetector(
-              onTap: onTap,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Icon(
-                    icon,
-                    color: Theme.of(context).primaryColor,  
-                  ),
-                ),
-              ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: backButton ?? false
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.end,
+            children: [
+              backButton ?? false
+                  ? Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: padding ?? 10.0, vertical: 10.0),
+                      child: TextButton(
+                        "Back",
+                        onClick: () {
+                          Navigator.of(context).pop();
+                        },
+                        iconLeft: true,
+                      ))
+                  : Container(),
+              icon == null
+                  ? Container()
+                  : Align(
+                      alignment: Alignment.centerRight,
+                      child: GestureDetector(
+                        onTap: onTap,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: padding ?? 10.0, vertical: 10.0),
+                          child: Icon(
+                            icon,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                    )
+            ],
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(left: 20),
+          padding: EdgeInsets.only(left: padding == null ? 20 : padding + 10),
           child: Container(
             width: double.infinity,
             child: Text(
@@ -39,7 +62,7 @@ class PageHeader extends StatelessWidget {
             ),
           ),
         ),
-      ], 
+      ],
     );
   }
 }
--- lib/pages/other/OrganisationPage.dart
+++ lib/pages/other/OrganisationPage.dart
@@ -1,7 +1,9 @@
 import 'package:app/assets/StyleFrom.dart';
-import 'package:app/assets/components/customAppBar.dart';
+import 'package:app/assets/components/header.dart';
 import 'package:app/assets/components/sectionTitle.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:app/models/Organisation.dart';
 
@@ -14,17 +16,26 @@ class OraganisationInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        text: "Partner",
-        context: context,
-      ),
       body: Column(
         children: <Widget>[
+
+          // Body
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              children: <Widget>[
+          PageHeader(
+            title: organisation.getName(),
+            backButton: true,
+            padding: 0,
+          ),
+
+          SizedBox(height: 10),
 
           Container(
             height: 120,
             width: double.infinity,
-            color: Colors.white,
             child: Padding(
               padding: EdgeInsets.all(10),
               child: Center(
@@ -32,29 +43,8 @@ class OraganisationInfoPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            color: Theme.of(context).primaryColor,
-            width: double.infinity,
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Container(
-                child: Text(
-                  organisation.getName(),
-                  style: textStyleFrom(
-                    Theme.of(context).primaryTextTheme.headline5,
-                    color: Colors.white,
-                  )
-                ),
-              )
-            )
-          ),
 
-          // Body
-          Expanded(
-            child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              children: <Widget>[
+
                 SizedBox(height: 10),
                 SectionTitle(
                   "About",
@@ -64,32 +54,141 @@ class OraganisationInfoPage extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
 
+
+                getSocialMediaChildren(organisation).length == 0 ? Container() :
+                SectionTitle(
+                  "Follow us on social media",
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.max,
+                  children: getSocialMediaChildren(organisation),
+                ),
+                getSocialMediaChildren(organisation).length == 0 ? Container() :
+                SizedBox(height: 15),
+
+                getExtraLinks(organisation).length == 0 ? Container() :
                 SectionTitle(
                   "Find out more",
                 ),
-                organisation.getWebsite() == null ? Container() :
-                Text(
-                  organisation.getWebsite(),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.max,
+                  children: getExtraLinks(organisation),
                 ),
-                organisation.getEmail() == null ? Container() :
-                Text(
-                  organisation.getEmail(),
-                ),
-                organisation.getInstagram() == null ? Container() :
-                Text(
-                  organisation.getInstagram(),
-                ),
-                organisation.getFacebook() == null ? Container() :
-                Text(
-                  organisation.getFacebook(),
-                ),
+
                 SizedBox(height: 20),
-                
+
               ],
             )
           )
         ],
       ),
     );
+  }
+}
+
+List<Widget> getSocialMediaChildren(Organisation org) {
+  List<Widget> socialButtons = [];
+  if (org.getInstagram() != null) {
+    socialButtons.add(SocialMediaButton(org.getInstagram(), FontAwesomeIcons.instagram));
+  }
+  if (org.getFacebook() != null) {
+    socialButtons.add(SocialMediaButton(org.getFacebook(), FontAwesomeIcons.facebookF));
+  }
+  if (org.getTwitter() != null) {
+    socialButtons.add(SocialMediaButton(org.getTwitter(), FontAwesomeIcons.twitter));
+  }
+  if (org.getEmail() != null) {
+    socialButtons.add(SocialMediaButton(org.getEmail(), FontAwesomeIcons.envelope));
+  }
+  if (org.getWebsite() != null) {
+    socialButtons.add(SocialMediaButton(org.getWebsite(), Icons.web));
+  }
+  return socialButtons;
+}
+
+class SocialMediaButton extends StatelessWidget {
+  final String link;
+  final IconData icon;
+  SocialMediaButton(this.link, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: IconButton(
+        icon: Icon(
+          icon,
+          color: Color.fromRGBO(155,159,177,1),
+          size: 35,
+        ),
+        onPressed: () {
+          launch(link);
+        },
+      ),
+    );
+  }
+}
+
+List<Widget> getExtraLinks(Organisation org) {
+  List<Widget> extraLinks = [];
+  if (org.getExtraText1() != null) {
+    extraLinks.add(ExtraLinkButton(org.getExtraText1(), org.getExtraLink1(), FontAwesomeIcons.externalLinkAlt));
+  }
+  if (org.getExtraText2() != null) {
+    extraLinks.add(ExtraLinkButton(org.getExtraText2(), org.getExtraLink2(), FontAwesomeIcons.externalLinkAlt));
+  }
+  if (org.getExtraText3() != null) {
+    extraLinks.add(ExtraLinkButton(org.getExtraText3(), org.getExtraLink3(), FontAwesomeIcons.externalLinkAlt));
+  }
+  return extraLinks;
+}
+
+class ExtraLinkButton extends StatelessWidget {
+  final String text;
+  final String link;
+  final IconData icon;
+  ExtraLinkButton(this.text, this.link, this.icon);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        launch(link);
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.3,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 45, 96, 0.08),
+              offset: Offset(0, 3),
+              blurRadius: 20,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Icon(
+                icon,
+                color: Color.fromRGBO(155,159,177,1),
+                size: 25,
+              ),
+              SizedBox(height: 10),
+              Text(
+                text,
+                style: textStyleFrom(
+                  Theme.of(context).primaryTextTheme.bodyText1,
+                  color: Color.fromRGBO(155,159,177,1),
+                ),
+              )
+            ],
+          ),
+        )
+    )
+  );
   }
 }
