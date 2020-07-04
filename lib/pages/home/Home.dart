705d6167dae8740017204e9ee66280c376941086
jelgar <jamesnelgar@gmail.com>
2020-07-04 09:52:38 +0100

    Ordered and shuffled campaigns on home page

--- lib/pages/home/Home.dart
+++ lib/pages/home/Home.dart
@@ -22,6 +22,7 @@ import 'package:app/assets/routes/customLaunch.dart';
 
 import 'package:app/models/ViewModel.dart';
 import 'package:app/models/State.dart';
+import 'package:app/models/Campaigns.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:redux/redux.dart';
@@ -250,108 +251,6 @@ Widget sectionTitle(String t, BuildContext context) {
   );
 }
 
-class HomeActionTile extends StatelessWidget {
-  final Function changePage;
-  HomeActionTile(this.changePage);
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-      },
-      child: StoreConnector<AppState, ViewModel>(
-        converter: (Store<AppState> store) => ViewModel.create(store),
-        builder: (BuildContext context, ViewModel viewModel) {
-          if (viewModel.userModel.user.getSelectedCampaigns() == null || viewModel.getActiveSelectedCampaings().getActions() == null) {
-            return CircularProgressIndicator();
-          }
-          else if (viewModel.getActiveSelectedCampaings().getActions().length == 0) {
-            return Container(
-              height: 200,
-              child: ViewCampaigns(),
-            );
-          }
-          else {
-            return Column(
-              children: <Widget>[
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: 3,
-                  itemBuilder: 
-                    (BuildContext context, int index) => 
-                      ActionSelectionItem(
-                        action: viewModel.campaigns.getActiveCampaigns()[index].getActions()[index],
-                        campaign: viewModel.campaigns.getActiveCampaigns()[index],
-                        outerHpadding: 10,
-                        backgroundColor: Colors.white,
-                    ),
-                ),
-                HomeButton(
-                  text: "All actions",
-                  changePage: changePage,
-                  page: TabPage.Actions,
-                )
-                //ActionItem(user.getFollowUpAction()),
-                //TODO Work ou where follow up actions should be
-              ]
-            );
-          }
-        }
-      )
-            
-    );
-  }
-}
-
-
-class HomeButton extends StatelessWidget {
-  final String text;
-  final Function changePage;
-  final TabPage page;
-
-  HomeButton({
-    @required this.text,
-    @required this.changePage,
-    this.page,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.all(BUTTON_PADDING),
-        child: 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget> [
-            DarkButton(
-              text, 
-              rightArrow: true,
-              onPressed: () {
-                changePage(
-                  page ?? TabPage.Home
-                );
-              },
-              style: DarkButtonStyles.Small,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomeDividor extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity, 
-      height: 2,
-      color: Color.fromRGBO(238,238,238, 1),
-    );
-  }
-}
-
 class CampaignCarosel extends StatelessWidget {
   final _controller = PageController(
     initialPage: 0,
@@ -365,6 +264,7 @@ class CampaignCarosel extends StatelessWidget {
         if (viewModel.campaigns.getActiveCampaigns() == null) {
           return CircularProgressIndicator();
         } else {
+          Campaigns cs = viewModel.getCampaignsWithSelctedFirst();
           return Column(
             children: [
               Container(
@@ -378,7 +278,7 @@ class CampaignCarosel extends StatelessWidget {
                       return Padding(
                         padding: EdgeInsets.symmetric(vertical: 10),
                         child: CampaignTile(
-                          viewModel.campaigns.getActiveCampaigns()[index],
+                          cs.getActiveCampaigns()[index],
                             // Used to only selected campaigns ==> remeber to change count
                         //viewModel
                         //  .userModel.user
