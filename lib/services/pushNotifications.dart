fa6db0dbd86910581ef0ea5b57cbf6359103df79
jelgar <jamesnelgar@gmail.com>
2020-07-17 20:25:35 +0100

    Added naviation to notifications - causing deeped rooted issues

--- lib/services/pushNotifications.dart
+++ lib/services/pushNotifications.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+import 'package:app/main.dart';
+
 class PushNotificationsService {
   final FirebaseMessaging _fcm  = FirebaseMessaging();
 
@@ -15,10 +17,21 @@ class PushNotificationsService {
       },
       onLaunch: (Map<String, dynamic> message) async {
         print('onLaunch: $message');
+        _serialiseAndNavigate(message);
       },
       onResume: (Map<String, dynamic> message) async {
         print('onResume: $message');
+        _serialiseAndNavigate(message);
       },
     );
+  }
+
+  void _serialiseAndNavigate(Map<String, dynamic> message) {
+    var notificationData = message['data'];
+    var view = notificationData['view'];
+
+    if (view != null) {
+      Keys.navKey.currentState.pushNamed(view);
+    }
   }
 }
