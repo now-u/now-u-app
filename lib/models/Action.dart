895d1d8e9eb2e14f54f91d380626734bd8a97a7f
jelgar <jamesnelgar@gmail.com>
2020-09-14 22:19:32 +0100

    Bug fix and changelog update

--- lib/models/Action.dart
+++ lib/models/Action.dart
@@ -2,7 +2,6 @@ import 'package:app/assets/StyleFrom.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // Custom Icons
 import 'package:app/assets/icons/customIcons.dart';
@@ -241,7 +240,7 @@ class CampaignAction {
     whatDescription = json['what_description'];
     whyDescription  = json['why_description'];
     link            = json['link'];
-    time            = json['time'].toDouble();
+    time            = json['time'] == null ? 5.0 : json['time'].toDouble();
     type            = campaignActionTypeFromString(json['type']);
     createdAt       = DateTime.parse(json['created_at']);
     releasedAt      = json['release_date'] == null ? null : DateTime.parse(json['release_date']);
