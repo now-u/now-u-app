abd6a9453dd1593b17a70cc4ca02f56450a364f8
Daniil Shumko <7705571+daniil-shumko@users.noreply.github.com>
2020-11-27 14:18:15 +0000

    Fixed ipa creation and added test fligh upload

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-xcrun altool --upload-app -t ios -f build/build/app/outputs/ios/release/app-release.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
+xcrun altool --upload-app -t ios -f "$IPA_PATH" -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
