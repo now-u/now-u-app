f20c4f100432bbb432daee0f8e46a2959cccbe46
Piotr Stanny <piotr.stanny@gmail.com>
2020-12-09 17:18:14 +0000

    Testing deleting with staging api

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -185,16 +185,23 @@ class AuthenticationService {
   Future<bool> deleteUserAccount() async {
     try {
       print("Delete method from Authentication Service fired");
-//      http.Response response = await http.delete(
-//        domainPrefix + 'users/me',
-//        headers: <String, String>{
-//          'Content-Type': 'application/json; charset=UTF-8',
+      switchToStagingBranch(); // Changed to staging api here
+      String testingToken = "3ac4e8d88113";
+      http.Response response = await http.delete(
+        domainPrefix + 'users/me',
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
 //          'token': currentUser.getToken(),
-//        },
-//      );
-//      if (response.statusCode != 200) {
-//        return false;
-//      }
+          'token': testingToken,
+        },
+      );
+      if (response.statusCode != 200) {
+        print("Returned false, no error but status not 200");
+        return false;
+      }
+      print("Returned true, account should be deleted");
+      print(response.body);
+      print(response.statusCode);
       return true;
     } catch (e) {
       print("Error");
