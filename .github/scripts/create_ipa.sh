43f35fe3ec64a97aee190bb71b4a417897cc599a
Daniil Shumko <7705571+daniil-shumko@users.noreply.github.com>
2021-01-08 11:39:03 +0000

    Building iOS app using new flutter build ipa command

--- .github/scripts/create_ipa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-xcodebuild -archivePath $PWD/build/Runner.xcarchive \
--exportOptionsPlist $PWD/ExportOptions.plist \
--exportPath $PWD/build \
--allowProvisioningUpdates \
--exportArchive | xcpretty
