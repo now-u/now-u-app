5efc9f3b33e4f218df9533b3ffdb4eeb13fe409d
Daniil Shumko <7705571+daniil-shumko@users.noreply.github.com>
2021-01-08 11:23:52 +0000

    Removed automatic build number setting

--- .github/scripts/build_ios.sh
+++ .github/scripts/build_ios.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $GITHUB_RUN_NUMBER" "Runner/Info.plist"
 xcodebuild -workspace Runner.xcworkspace \
 -scheme Runner \
 -sdk iphoneos \
