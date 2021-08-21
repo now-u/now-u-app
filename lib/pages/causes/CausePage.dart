75387aa303b2e2c5285ce2554f8ee99fdf15b552
charlieblindsay <charlieblindsay@gmail.com>
2021-08-21 09:16:50 +0100

    added the viewmodel and navigation/causesList

--- lib/pages/causes/CausePage.dart
+++ lib/pages/causes/CausePage.dart
@@ -1,43 +1,74 @@
+import 'package:app/viewmodels/causes_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:app/assets/components/causeTile.dart';
 import 'package:app/models/Cause.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:app/assets/components/buttons/customWidthButton.dart';
+import 'package:app/viewmodels/causes_view_model.dart';
+import 'package:stacked/stacked.dart';
 
 class CausePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('Welcome to now-u'),
-          Text('Take action and selected the causes which are important to you'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              CauseTile(cause: Cause(name: 'Environment'), causeIcon: FontAwesomeIcons.leaf, isSelected: false),
-              CauseTile(cause: Cause(name: 'Health & Wellbeing'), causeIcon: FontAwesomeIcons.heart, isSelected: true),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              CauseTile(cause: Cause(name: 'Environment'), causeIcon: FontAwesomeIcons.leaf, isSelected: false),
-              CauseTile(cause: Cause(name: 'Health & Wellbeing'), causeIcon: FontAwesomeIcons.heart, isSelected: true),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              CauseTile(cause: Cause(name: 'Environment'), causeIcon: FontAwesomeIcons.leaf, isSelected: false),
-              CauseTile(cause: Cause(name: 'Health & Wellbeing'), causeIcon: FontAwesomeIcons.heart, isSelected: true),
-            ],
-          ),
-          CustomWidthButton('Get started', onPressed: () {print('hello');}, size: ButtonSize.Medium, fontSize: 20.0, buttonWidthProportion: 0.6)
-        ],
-      ),
-    );
+        body: ViewModelBuilder<CausesViewModel>.reactive(
+            viewModelBuilder: () => CausesViewModel(),
+            onModelReady: (model) => model.fetchCauses(),
+            builder: (context, model, child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Welcome to now-u'),
+                  Text(
+                      'Take action and selected the causes which are important to you'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CauseTile(
+                          cause: model.causesList[0],
+                          causeIcon: FontAwesomeIcons.leaf,
+                          isSelected: false),
+                      CauseTile(
+                          cause: model.causesList[1],
+                          causeIcon: FontAwesomeIcons.heart,
+                          isSelected: false),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CauseTile(
+                          cause: model.causesList[0],
+                          causeIcon: FontAwesomeIcons.leaf,
+                          isSelected: false),
+                      CauseTile(
+                          cause: model.causesList[1],
+                          causeIcon: FontAwesomeIcons.heart,
+                          isSelected: true),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CauseTile(
+                          cause: model.causesList[0],
+                          causeIcon: FontAwesomeIcons.leaf,
+                          isSelected: false),
+                      CauseTile(
+                          cause: model.causesList[1],
+                          causeIcon: FontAwesomeIcons.heart,
+                          isSelected: true),
+                    ],
+                  ),
+                  CustomWidthButton('Get started',
+                      onPressed: () {
+                    model.goHome();
+                  },
+                      size: ButtonSize.Medium,
+                      fontSize: 20.0,
+                      buttonWidthProportion: 0.6)
+                ],
+              );
+            }));
   }
 }
---
+++ lib/viewmodels/causes_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:app/routes.dart';
+import 'package:app/viewmodels/base_model.dart';
+import 'package:app/models/Cause.dart';
+import "dart:async";
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:app/services/navigation_service.dart';
+import 'package:app/locator.dart';
+
+
+class CausesViewModel extends BaseModel{
+
+  List<Cause> _causesList;
+  List<Cause> get causesList => _causesList;
+
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  Future fetchCauses() async{
+    await Future.delayed(const Duration(seconds : 5));
+    _causesList = [
+      Cause(name: 'Environment'),
+      Cause(name: 'Health & Wellbeing')
+    ];
+    notifyListeners();
+  }
+
+  void goHome() async{
+    _navigationService.navigateTo(Routes.home);
+  }
+}
