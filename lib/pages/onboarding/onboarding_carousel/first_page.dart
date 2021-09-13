9ebdbc13c35424eaf3884bd089d08f3fffa83589
Ahmed Tilal OSMAN <79141373+ahmedtilal@users.noreply.github.com>
2021-09-13 22:27:45 +0100

    Update onboarding pages to match new designs. (#167)
    * Update onboarding pages to match new designs.
    
    * Removing unused code in onboarding viewModel and refactoring carousel pages code.
    
    * Refactor Carousel Pages.

---
+++ lib/pages/onboarding/onboarding_carousel/first_page.dart
@@ -0,0 +1,33 @@
+import 'package:app/pages/onboarding/onboarding_carousel/basic_carousel_page.dart';
+import 'package:app/viewmodels/onboarding_model.dart';
+import 'package:flutter/material.dart';
+
+class FirstOnBoardingPage extends StatelessWidget {
+  const FirstOnBoardingPage({this.model, Key key}) : super(key: key);
+  final OnBoardingViewModel model;
+  @override
+  Widget build(BuildContext context) {
+    return BasicCarouselPage(
+        model: model,
+        boldText: 'Welcome',
+        normalText: ' Our mission is to',
+        additionalText: [
+          TextSpan(
+            text: ' inform, involve ',
+            style: TextStyle(fontWeight: FontWeight.w800),
+          ),
+          TextSpan(text: 'and '),
+          TextSpan(
+            text: 'inspire ',
+            style: TextStyle(fontWeight: FontWeight.w800),
+          ),
+          TextSpan(
+              text:
+                  'everyone to help tackle some of the world’s most pressing problems.')
+        ],
+        backgroundImagePath: 'assets/imgs/intro/OnBoarding1.png',
+        logoPath: 'assets/imgs/intro/now-u-logo-onboarding.png',
+        showSkipButton: false,
+        showGetStartedButton: false);
+  }
+}
