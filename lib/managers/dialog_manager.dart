61782f10146395689bd1b650e6294076ad505302
jelgar <jamesnelgar@gmail.com>
2021-11-08 18:56:41 +0000

    Trying multiclass approach to dialogs

--- lib/managers/dialog_manager.dart
+++ lib/managers/dialog_manager.dart
@@ -16,67 +16,22 @@ class DialogManager extends StatefulWidget {
   _DialogManagerState createState() => _DialogManagerState();
 }
 
-class _DialogManagerState extends State<DialogManager> {
-  final DialogService _dialogService = locator<DialogService>();
+abstract class CustomDialog {
+  final String title;
+  final String? description;
+  final List? buttons;
 
-  @override
-  void initState() {
-    super.initState();
-    _dialogService.registerDialogListener(_showDialog);
-  }
+  CustomDialog({
+    required this.title,
+    this.description,
+    this.buttons,
+  });
 
-  @override
-  Widget build(BuildContext context) {
-    return widget.child!;
-  }
+  void show(BuildContext context);
+  
+  List<Widget> _getButtons(List? buttons, BuildContext context) {
+    DialogService _dialogService = locator<DialogService>();
 
-  void _showDialog(AlertRequest request) {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (_) => AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        content: Padding(
-            padding: EdgeInsets.all(25),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(height: 20),
-                Text(
-                  request.title!,
-                  textAlign: TextAlign.center,
-                  style: textStyleFrom(
-                    Theme.of(context).primaryTextTheme.headline2,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Text(
-                    request.description!,
-                    style: textStyleFrom(
-                      Theme.of(context).primaryTextTheme.bodyText1,
-                      color: Theme.of(context).primaryColorDark,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Column(
-                  children: _getButtons(request.buttons),
-                ),
-                SizedBox(height: 20),
-              ],
-            ),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _getButtons(List? buttons) {
     if (buttons == null) {
       return <Widget>[
         Container(
@@ -113,4 +68,81 @@ class _DialogManagerState extends State<DialogManager> {
     }
     return buttonWidgets;
   }
+}
+
+class BasicDialog extends CustomDialog {
+  String headerImage;
+  BasicDialog({
+    required String title,
+    required String description,
+    List? buttons,
+
+    required this.headerImage,
+  }) : super(title: title, description: description, buttons: buttons);
+
+  void show(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        content: Padding(
+            padding: EdgeInsets.all(25),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: textStyleFrom(
+                    Theme.of(context).primaryTextTheme.headline2,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text(
+                    description,
+                    style: textStyleFrom(
+                      Theme.of(context).primaryTextTheme.bodyText1,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Column(
+                  children: _getButtons(request.buttons, context),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
+
+class _DialogManagerState extends State<DialogManager> {
+  final DialogService _dialogService = locator<DialogService>();
+
+  @override
+  void initState() {
+    super.initState();
+    _dialogService.registerDialogListener(_showDialog);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child!;
+  }
+
+  void _showDialog(CustomDialog dialog) {
+    dialog.show(context);
+  }
+
 }
