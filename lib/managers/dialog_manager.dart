be205480e1717a1b35cb5087b66454bf6cf56509
jelgar <jamesnelgar@gmail.com>
2021-11-14 00:25:35 +0000

    Wanting to do muliple classes again

--- lib/managers/dialog_manager.dart
+++ lib/managers/dialog_manager.dart
@@ -1,4 +1,5 @@
 import 'package:app/assets/components/buttons/customWidthButton.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app/locator.dart';
@@ -9,29 +10,83 @@ import 'package:app/assets/StyleFrom.dart';
 
 
 class DialogManager extends StatefulWidget {
-  final Widget? child;
-  DialogManager({Key? key, this.child}) : super(key: key);
+  final Widget child;
+  DialogManager({Key? key, required this.child}) : super(key: key);
 
   @override
   _DialogManagerState createState() => _DialogManagerState();
 }
 
-abstract class CustomDialog {
-  final String title;
-  final String? description;
-  final List? buttons;
+class _DialogManagerState extends State<DialogManager> {
+  final DialogService _dialogService = locator<DialogService>();
 
-  CustomDialog({
-    required this.title,
-    this.description,
-    this.buttons,
-  });
+  @override
+  void initState() {
+    super.initState();
+    _dialogService.registerDialogListener(_showDialog);
+  }
 
-  void show(BuildContext context);
-  
-  List<Widget> _getButtons(List? buttons, BuildContext context) {
-    DialogService _dialogService = locator<DialogService>();
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
 
+  void _showDialog(AlertRequest request) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) => AlertDialog(
+        contentPadding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        clipBehavior: Clip.hardEdge,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if(request.headerImage != null) CachedNetworkImage(imageUrl: request.headerImage!),
+            SizedBox(height: 15),
+            Padding(
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      request.title!,
+                      textAlign: TextAlign.center,
+                      style: textStyleFrom(
+                        Theme.of(context).primaryTextTheme.headline2,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        request.description!,
+                        style: textStyleFrom(
+                          Theme.of(context).primaryTextTheme.bodyText1,
+                          color: Theme.of(context).primaryColorDark,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Column(
+                      children: _getButtons(request.buttons),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+            ),
+          ]
+        )
+      ),
+    );
+  }
+
+  List<Widget> _getButtons(List? buttons) {
     if (buttons == null) {
       return <Widget>[
         Container(
@@ -68,81 +123,4 @@ abstract class CustomDialog {
     }
     return buttonWidgets;
   }
-}
-
-class BasicDialog extends CustomDialog {
-  String headerImage;
-  BasicDialog({
-    required String title,
-    required String description,
-    List? buttons,
-
-    required this.headerImage,
-  }) : super(title: title, description: description, buttons: buttons);
-
-  void show(BuildContext context) {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (_) => AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        content: Padding(
-            padding: EdgeInsets.all(25),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(height: 20),
-                Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  style: textStyleFrom(
-                    Theme.of(context).primaryTextTheme.headline2,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Text(
-                    description,
-                    style: textStyleFrom(
-                      Theme.of(context).primaryTextTheme.bodyText1,
-                      color: Theme.of(context).primaryColorDark,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Column(
-                  children: _getButtons(request.buttons, context),
-                ),
-                SizedBox(height: 20),
-              ],
-            ),
-        ),
-      ),
-    );
-  }
-}
-
-class _DialogManagerState extends State<DialogManager> {
-  final DialogService _dialogService = locator<DialogService>();
-
-  @override
-  void initState() {
-    super.initState();
-    _dialogService.registerDialogListener(_showDialog);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.child!;
-  }
-
-  void _showDialog(CustomDialog dialog) {
-    dialog.show(context);
-  }
-
 }
--- lib/viewmodels/startup_model.dart
+++ lib/viewmodels/startup_model.dart
@@ -38,7 +38,7 @@ class StartUpViewModel extends BaseModel {
     var hasLoggedInUser = await _authenticationService!.isUserLoggedIn();
 
     if (hasLoggedInUser) {
-      _navigationService!.navigateTo(Routes.home);
+      _navigationService!.navigateTo(Routes.onBoarding);
     } else {
       _navigationService!.navigateTo(Routes.onBoarding);
     }
