c4bb7649c6b8d0637d2de874b28d6c13185c9a0a
jelgar <jamesnelgar@gmail.com>
2022-12-03 17:45:30 +0000

    Add ensureInitialized to shared preferences test

--- test/service_tests/shared_preferences_service_test.dart
+++ test/service_tests/shared_preferences_service_test.dart
@@ -2,6 +2,8 @@ import 'package:app/services/shared_preferences_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('shared preferences service', () {
     test('sets and gets value', () async {
       SharedPreferencesService sharedPreferencesService = SharedPreferencesService();
