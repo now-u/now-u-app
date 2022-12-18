9051998707ead61b29035f5677f370575b5337b6
jelgar <jamesnelgar@gmail.com>
2022-12-18 23:47:41 +0000

    Fix tests

--- test/setup/test_helpers.dart
+++ test/setup/test_helpers.dart
@@ -1,12 +1,6 @@
 import 'package:mockito/mockito.dart';
 import 'dart:io';
-
 import 'package:app/locator.dart';
-import 'package:app/services/navigation_service.dart';
-import 'package:app/services/dialog_service.dart';
-
-import 'package:app/services/analytics.dart';
-import 'package:app/services/google_location_search_service.dart';
 
 Map<String, String> unauthenticatedHeaders = {
   "Content-Type": "application/json; charset=UTF-8"
@@ -16,47 +10,6 @@ Future<String> readTestData(String fileName,
     {String filePath = "assets/json/"}) async {
   final file = File(filePath + fileName);
   return await file.readAsString();
-}
-
-class MockNavigationService extends Mock implements NavigationService {
-  Future<dynamic> navigateTo(String route, {arguments}) async {
-    return route;
-  }
-}
-
-NavigationService getAndRegisterMockNavigationService() {
-  _removeRegistrationIfExists<NavigationService>();
-  var service = MockNavigationService();
-  locator.registerSingleton<NavigationService>(service);
-  return service;
-}
-
-class MockDialogService extends Mock implements DialogService {}
-
-DialogService getAndRegisterMockDialogService() {
-  _removeRegistrationIfExists<DialogService>();
-  var service = MockDialogService();
-  locator.registerSingleton<DialogService>(service);
-  return service;
-}
-
-class MockGoogleLocationSearchService extends Mock
-    implements GoogleLocationSearchService {}
-
-GoogleLocationSearchService getAndRegisterMockGoogleLocationSearchService() {
-  _removeRegistrationIfExists<GoogleLocationSearchService>();
-  var service = MockGoogleLocationSearchService();
-  locator.registerSingleton<GoogleLocationSearchService>(service);
-  return service;
-}
-
-class MockAnalyticsService extends Mock implements AnalyticsService {}
-
-AnalyticsService getAndRegisterMockAnalyticsService() {
-  _removeRegistrationIfExists<AnalyticsService>();
-  var service = MockAnalyticsService();
-  locator.registerSingleton<AnalyticsService>(service);
-  return service;
 }
 
 void _removeRegistrationIfExists<T extends Object>() {
