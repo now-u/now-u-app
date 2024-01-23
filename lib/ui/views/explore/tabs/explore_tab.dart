c154dc6206a29a5ee68d7485f5c34be4b77c5def
Kacper Kogut <kacper.kogut16@gmail.com>
2024-01-23 23:15:23 +0100

    Remove doubled explore tab

--- lib/ui/views/explore/tabs/explore_tab.dart
+++ lib/ui/views/explore/tabs/explore_tab.dart
@@ -3,30 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../paging/paging_state.dart';
 import 'filters_container.dart';
 
-class ExploreTabHorizontal extends StatelessWidget {
-  final Iterable<Widget> filterChips;
-  final Iterable<Widget> filterResults;
-
-  const ExploreTabHorizontal({
-    required this.filterChips,
-    required this.filterResults,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: filterResults.length + 1,
-      itemBuilder: (BuildContext context, int index) {
-        if (index == 0) {
-          return FiltersContainer(filterChips: filterChips);
-        } else {
-          return filterResults.toList()[index - 1];
-        }
-      },
-    );
-  }
-}
-
 class ExploreTab<T> extends StatelessWidget {
   final Iterable<Widget> filterChips;
   final PagingState<T> pagingState;
