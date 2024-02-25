58ef53063710abcd0e236f562a31f0b41ab68ebd
Kacper Kogut <kacper.kogut16@gmail.com>
2024-02-25 21:43:05 +0100

    Update path provider and font awesome

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -21,7 +21,6 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "actionBack": MessageLookupByLibrary.simpleMessage("Back"),
         "actionSave": MessageLookupByLibrary.simpleMessage("Save"),
