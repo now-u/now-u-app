65b750fed22fb510ce7438cc983cc4aee3b4f792
Kacper Kogut <kacper.kogut16@gmail.com>
2024-07-23 22:35:53 +0200

    Fix causes service tests

--- test/service_tests/causes_service_test.dart
+++ test/service_tests/causes_service_test.dart
@@ -1,15 +1,21 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:causeApiClient/causeApiClient.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:nowu/services/analytics.dart';
 import 'package:nowu/services/api_service.dart';
 import 'package:nowu/services/auth.dart';
-import 'package:nowu/services/causes_service.dart';
+import 'package:nowu/services/causes_service.dart' hide Cause;
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:test/test.dart';
 
 class MockAuthenticationService extends Mock implements AuthenticationService {
   @override
   bool get isAuthenticated => false;
+
+  @override
+  Stream<AuthState> get authState => const Stream.empty();
 }
 
 class MockAnalyticsService extends Mock implements AnalyticsService {}
@@ -32,6 +38,25 @@ class MockApiService extends Mock implements ApiService {
 
   @override
   MockApiClient get apiClient => client;
+}
+
+class MockCause extends Mock implements Cause {
+  @override
+  int get id => 1;
+
+  @override
+  String get title => 'Test Cause';
+
+  @override
+  String get description => 'Test Description';
+
+  @override
+  Image get headerImage => Image((b) => b
+    ..id = 2
+    ..url = 'https://example.com/image.jpg');
+
+  @override
+  IconEnum get icon => IconEnum.economicOpportunity;
 }
 
 void main() {
@@ -57,10 +82,22 @@ void main() {
     });
 
     test('Get causes', () async {
+      when(() => mockApiClient.getCausesApi().causesList()).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(path: ''),
+          data: BuiltList<Cause>(
+            [
+              MockCause(),
+            ],
+          ),
+          statusCode: 200,
+        ),
+      );
+
       final causesService = locator<CausesService>();
       final causes = await causesService.listCauses();
+      // verify(mockApiClient.getCausesApi().causesList());
 
-      verify(mockApiClient.getCausesApi().causesList());
       expect(causes.length, greaterThan(0));
     });
   });
