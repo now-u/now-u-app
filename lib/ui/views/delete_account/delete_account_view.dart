a647f3a598341b4c0431baff26ffd499f795399e
Kacper Kogut <kacper.kogut16@gmail.com>
2024-08-08 22:01:29 +0200

    Extract delete account button to separate widget

--- lib/ui/views/delete_account/delete_account_view.dart
+++ lib/ui/views/delete_account/delete_account_view.dart
@@ -13,53 +13,69 @@ class DeleteAccountView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => DeleteAccountBloc(
-        authenticationService: locator<AuthenticationService>(),
-        appRouter: AutoRouter.of(context),
-        userService: locator<UserService>(),
-      ),
-      child: BlocBuilder<DeleteAccountBloc, DeleteAccountState>(
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Delete Account'),
-            ),
-            body: Container(
-              color: Colors.white,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Are you sure you want to delete your account? This action cannot be undone.',
-                        style: Theme.of(context).textTheme.titleMedium,
-                        textAlign: TextAlign.center,
-                      ),
-                      const Padding(padding: EdgeInsets.all(8)),
-                      TextField(
-                        onChanged:
-                            context.read<DeleteAccountBloc>().updateInputName,
-                        decoration: const InputDecoration(
-                          hintText: 'Type account name to confirm',
-                        ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(8)),
-                      ElevatedButton(
-                        onPressed: state.isNameValid
-                            ? context.read<DeleteAccountBloc>().deleteAccount
-                            : null,
-                        child: Text('Delete Account'),
-                      ),
-                    ],
+      create: (_) =>
+          DeleteAccountBloc(
+            authenticationService: locator<AuthenticationService>(),
+            appRouter: AutoRouter.of(context),
+            userService: locator<UserService>(),
+          ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Delete Account'),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Are you sure you want to delete your account? This action cannot be undone.',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleMedium,
+                    textAlign: TextAlign.center,
                   ),
-                ),
+                  const Padding(padding: EdgeInsets.all(8)),
+                  TextField(
+                    onChanged:
+                    context
+                        .read<DeleteAccountBloc>()
+                        .updateInputName,
+                    decoration: const InputDecoration(
+                      hintText: 'Type account name to confirm',
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(8)),
+
+                  DeleteAccountButton(),
+                ],
               ),
             ),
-          );
-        },
-      ),
+          ),
+        ),
+      );,
+    );
+  }
+}
+
+class DeleteAccountButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<DeleteAccountBloc, DeleteAccountState>(
+      builder: (context, state) {
+        return ElevatedButton(
+          onPressed: state.isNameValid
+              ? context
+              .read<DeleteAccountBloc>()
+              .deleteAccount
+              : null,
+          child: const Text('Delete Account'),
+        );
+      },
     );
   }
 }
