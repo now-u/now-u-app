2eee335feea2a4f59b4926d4fd49cd481c73e1dc
Kacper Kogut <kacper.kogut16@gmail.com>
2024-09-18 22:43:30 +0200

    Fix time filter brackets

--- lib/models/time.dart
+++ lib/models/time.dart
@@ -1,9 +1,9 @@
 const List<({String text, double minTime, double maxTime})> timeBrackets = [
-  (text: '1-5 mins', minTime: 0, maxTime: 5),
-  (text: '5-15 mins', minTime: 5, maxTime: 15),
-  (text: '15-30 mins', minTime: 15, maxTime: 30),
-  (text: '30-60 mins', minTime: 30, maxTime: 60),
-  (text: 'Few hours', minTime: 60, maxTime: 240),
+  (text: '1-5 mins', minTime: 0, maxTime: 4.9),
+  (text: '5-15 mins', minTime: 5, maxTime: 14.9),
+  (text: '15-30 mins', minTime: 15, maxTime: 29.9),
+  (text: '30-60 mins', minTime: 30, maxTime: 59.9),
+  (text: 'Few hours', minTime: 60, maxTime: 239.9),
   (text: 'Long term', minTime: 240, maxTime: double.infinity),
 ];
 
